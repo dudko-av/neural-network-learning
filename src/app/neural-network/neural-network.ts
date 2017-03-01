895d339558d5a5d5da4557a209c79c055438110e
Alexander <oleksandr.dudko@customertimes.com>
2017-03-01 10:26:01 +0200

    add implementation link

--- src/app/neural-network/neural-network.ts
+++ src/app/neural-network/neural-network.ts
@@ -1,3 +1,7 @@
+/*
+ https://medium.com/technology-invention-and-more/how-to-build-a-simple-neural-network-in-9-lines-of-python-code-cc8f23647ca1#.f71ac5560
+ */
+
 declare const math: any;
 
 export class NeuralNetwork {
